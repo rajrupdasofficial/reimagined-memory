45a2f8bdeac4e9aca612e5423ee3cd4f03ee4b85
RajrupDasid <rajrupdasid@keemail.me>
2023-03-08 00:58:58 +0530

    websocket for online user created as of now

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const cors = require ('cors');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
-
+const ws = require('ws');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -59,7 +59,7 @@ app.post('/login', async (req,res)=>{
         const passOk = bcrypt.compare(password,findUser.password)
         if(passOk){
             jwt.sign({userId: findUser._id, username}, jwtSecret,{},(err,token)=>{
-                res.cookie('token',token).json({
+                res.cookie('token',token,{sameSite:'none',secure:true}).json({
                     id: findUser._id,
                 });
             });
@@ -67,4 +67,32 @@ app.post('/login', async (req,res)=>{
     }
 });
 
-app.listen(3999);
+const server = app.listen(3999);
+
+const wss = new ws.WebSocketServer({server});
+wss.on('connection',(connection, req)=>{
+   const cookies =req.headers.cookie;
+   if (cookies){
+    const tokenCookieString = cookies.split(';').find(str=>str.startsWith('token='));
+    if (tokenCookieString){
+        const token = tokenCookieString.split('=')[1];
+        if (token){
+            jwt.verify(token,jwtSecret,{},(err,userData)=>{
+            
+                if(err) throw err;
+                const {userId, username} = userData;
+                connection.userId = userId;
+                connection.username = username;
+            });
+        }
+    }
+   }
+
+   [...wss.clients].forEach(client=>{
+        client.send(JSON.stringify({
+            online:[...wss.clients].map(c=>({userId:c.userId,username:c.username})),
+        }
+            
+        ));
+   });
+});
