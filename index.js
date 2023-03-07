38d2b42c04285697d502dd99b404b89420cb05f3
RajrupDasid <rajrupdasid@keemail.me>
2023-03-07 12:46:35 +0530

    added register functionalities with cors router

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 const express = require('express');
 const mongodb = require('mongoose');
-const User=require('./models/User');
+const User = require('./models/User');
 const jwt = require('jsonwebtoken');
+const cors = require ('cors');
 
 const dotenv = require('dotenv');
 dotenv.config();
-//console.log(process.env.MONGO_URL);
-mongodb.connect(process.env.MONGO_URL);
-const jwtSecret = process.env.JWT_SECRET;
-const app = express();
 
-app.get('/test',(req,res)=>{
+mongodb.connect(process.env.MONGO_URL);
+
+const jwtSecret = process.env.JWT_SECRET;
+
+const app = express();
+app.use(cors({
+    credentials:true,
+    origin:process.env.CLIENT_URL,
+}));
+
+app.use(express.json());
+
+app.get('/test', (req, res) => {
     res.json('test ok');
 });
 
-app.post('/register', async(req,res)=>{
-    const {username,password} =req.body;
-    const createdUser = await User.create({username,password});
-    jwt.sign({userId:createdUser._id},jwtSecret,(err,token)=>{
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const createdUser = await User.create({ username, password });
+    jwt.sign({ userId: createdUser._id }, jwtSecret, (err, token) => {
         if (err) throw err;
-        res.cookie('token',token).status(201).json('ok');
+        res.cookie('token', token).status(201).json('ok');
     });
 });
 
