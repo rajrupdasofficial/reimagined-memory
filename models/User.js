2cb3b52ceffe8465e17f14c8c445b671b39ec09a
RajrupDasid <rajrupdasid@keemail.me>
2023-03-07 12:47:05 +0530

    added password hashers for password salting

--- models/User.js
+++ models/User.js
@@ -1,7 +1,22 @@
-const mongodb =require('mongoose');
-const UserSchema = new mongodb.Schema({
-    username: {type:String, unique:true},
-    password: String,
-},{timestamps:true});
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-export const UserModel = mongodb.model('User',UserSchema);
+const UserSchema = new mongoose.Schema({
+    username: { type: String, unique: true },
+    password: { type: String, required: true },
+}, { timestamps: true });
+
+UserSchema.pre('save', async function (next) {
+    const user = this;
+    if (!user.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(16);
+    const hash = await bcrypt.hash(user.password, salt);
+    user.password = hash;
+    next();
+});
+
+const UserModel = mongoose.model('User', UserSchema);
+
+module.exports = UserModel;
