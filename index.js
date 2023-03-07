e8648775bb837c4ede690916bc85dd685ff775a2
RajrupDasid <rajrupdasid@keemail.me>
2023-03-07 21:10:22 +0530

    register user and login user creation hasbeen rectified and now working fine

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongodb = require('mongoose');
 const User = require('./models/User');
 const jwt = require('jsonwebtoken');
 const cors = require ('cors');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -12,6 +14,7 @@ mongodb.connect(process.env.MONGO_URL);
 const jwtSecret = process.env.JWT_SECRET;
 
 const app = express();
+app.use(cookieParser());
 app.use(cors({
     credentials:true,
     origin:process.env.CLIENT_URL,
@@ -19,17 +22,49 @@ app.use(cors({
 
 app.use(express.json());
 
+
 app.get('/test', (req, res) => {
     res.json('test ok');
+});
+
+app.get('/profile',(req,res)=>{
+   const token =  req.cookies?.token;
+   if(token){
+   jwt.verify(token,jwtSecret,{},(err, userData)=>{
+    if(err) throw err;
+    res.json(
+        userData
+    );
+   });
+    }else{
+        res.status(401).json('no token');
+    }
 });
 
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
     const createdUser = await User.create({ username, password });
-    jwt.sign({ userId: createdUser._id }, jwtSecret, (err, token) => {
+    jwt.sign({ userId: createdUser._id, username}, jwtSecret, {},(err, token) => {
         if (err) throw err;
-        res.cookie('token', token).status(201).json('ok');
+        res.cookie('token', token,{sameSite:'none',secure:true}).status(201).json({
+            id: createdUser._id,
+        });
     });
+});
+
+app.post('/login', async (req,res)=>{
+    const{username, password}=req.body;
+   const findUser = await User.findOne({username});
+   if(findUser){
+        const passOk = bcrypt.compare(password,findUser.password)
+        if(passOk){
+            jwt.sign({userId: findUser._id, username}, jwtSecret,{},(err,token)=>{
+                res.cookie('token',token).json({
+                    id: findUser._id,
+                });
+            });
+        }
+    }
 });
 
 app.listen(3999);
